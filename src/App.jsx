46d5c5b3281afaa7cd78130d83b45ae7fb8edf7d
Minh Tan <tanprovip156@gmail.com>
2024-03-14 22:23:20 +0700

    config_theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Button from '@mui/material/Button'
-
+import Typography  from '@mui/material/Typography'
 function App() {
  
 
   return (
     <>
       <div>MinhTan</div>
+      <Typography variant= "body2" color = "text.secondary"> testtt </Typography>
+
       <Button variant='text'>Text</Button>
       <Button variant='contained'>Contained</Button>
       <Button variant='outlined'>Outlined</Button>
