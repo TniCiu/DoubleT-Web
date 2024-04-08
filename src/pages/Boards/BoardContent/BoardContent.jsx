b0900180c2cab2d63190fd25332a278f86834d80
Minh Tan <tanprovip156@gmail.com>
2024-04-08 20:53:10 +0700

    Drag_Drop_Card_p2

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -6,6 +6,7 @@ import { arrayMove } from '@dnd-kit/sortable'
 import { useEffect, useState } from 'react'
 import Column from './ListColumns/Column/Column'
 import Card from './ListColumns/Column/ListCards/Card/Card'
+import { cloneDeep } from 'lodash'
 
 const ACTIVE_DRAG_ITEM_TYPE = {
   COLUMN : 'ACTIVE_DRAG_ITEM_TYPE_COLUMN',
@@ -30,6 +31,10 @@ function BoardContent({ board }) {
     setOrderedColumnsState(orderedColumns)
   }, [board])
 
+  const findColumnByCardId = (cardId) => {
+    return orderedColumnsState.find(column => column?.cards?.map(card => card._id)?.includes(cardId))
+  }
+
   const handleDragStart = (event) => {
     console.log('handleDragStart:', event)
     setactiveDragItemId(event?.active?.id)
@@ -37,11 +42,65 @@ function BoardContent({ board }) {
       ACTIVE_DRAG_ITEM_TYPE.COLUMN)
     setactiveDragItemData(event?.active?.data?.current)
   }
+  // qua trinh keo 1 phan tu
+  const handleDragOver = (event) => {
+    // ko lam gi khi dang keo col
+    if (activeDragItemType === ACTIVE_DRAG_ITEM_TYPE.COLUMN ) return
+    // xu li neu keo card giua cac col
+    const { active, over } = event
+    if ( !active ||!over) return
+    // over la cai card Dang tuong tac
+    const { id : activeDraggingCardId, data : { current :activeDraggingCardData } } = active
+    const { id : overCardId } = over
+    // tim 2 cai col theo card
+    const activeColumn = findColumnByCardId(activeDraggingCardId)
+    const overColumn = findColumnByCardId(overCardId)
+    // neu ton tai 1 trong 2 col thi k lam gi het
+    if (!activeColumn || !overColumn) return
+
+
+    if (activeColumn._id !== overColumn._id) {
+      setOrderedColumnsState(prevColumns => {
+        // tim vi tri trong col noi card sap duoc tha
+        const overCardIndex = overColumn?.cards?.findIndex(card => card._id === overCardId)
+        // logic tinh toan card index moi moi khi dc keo'
+        let newCardIndex
+        const isBelowOverItem = active.rect.current.translated &&
+            active.rect.current.translated.top > over.rect.top + over.rect.height
+        const modifier = isBelowOverItem ? 1 :0
+
+        newCardIndex = overCardIndex >= 0 ? overCardIndex + modifier : overColumn.length + 1
+
+        // clone mang cu~ OrderCol ra 1 cai roi xu ly data , return de cap nhat lai vi tri
+        const nextColumns = cloneDeep(prevColumns)
+        const nextActiveColumn = nextColumns.find(column => column._id === activeColumn._id)
+        const nextOverColumn = nextColumns.find(column => column._id === overColumn._id)
+        if (nextActiveColumn) {
+          // xoa card o Col cu~
+          nextActiveColumn.cards=nextActiveColumn.cards.filter(card => card._id !== activeDraggingCardId)
+          // cap nhat lai mang sau khi xoa
+          nextActiveColumn.cardOrderIds= nextActiveColumn.cards.map( card => card._id)
+        }
+        if (nextOverColumn) {
+          // kiem tra xem card đã tồn tại trước đó trong col hay chưa
+          nextOverColumn.cards=nextOverColumn.cards.filter(card => card._id !== activeDraggingCardId)
+          // thêm card đã kéo vào col ở vị trí index mới 
+          nextOverColumn.cards = nextOverColumn.cards.toSpliced(newCardIndex, 0, activeDraggingCardData)
+          // cap nhat lai du lieu
+          nextOverColumn.cardOrderIds = nextOverColumn.cards.map( card => card._id)
+        }
+        return nextColumns
+      })
+    }
+  }
 
   const handleDragEnd = (event) => {
     console.log('handleDragEnd:', event)
+    if (activeDragItemType === ACTIVE_DRAG_ITEM_TYPE.CARD) {
+      return
+    }
     const { active, over } = event
-    if (!over) return
+    if ( !active ||!over) return
 
 
     if (active.id !== over.id) {
@@ -74,6 +133,7 @@ function BoardContent({ board }) {
       onDragEnd={handleDragEnd}
       sensors = {sensors}
       onDragStart={handleDragStart}
+      onDragOver={ handleDragOver}
     >
       <Box sx = {{
         bgcolor : (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#0984e3' ),
