12dfdc59dc720a78d176fd335d44a21cda2c153f
TniCiu <tindoris29@gmai.com>
2024-04-17 16:03:56 +0700

    Init base api project

---
+++ src/config/mongodb.js
@@ -0,0 +1,6 @@
+//tincui012
+//JEieHKKFOskM8AlP  
+
+const MONGODB_URI = 'mongodb+srv://tincui012:Tin2003@cluster0-tniciu.zokbtjy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0-TniCiu'
+
+const DATABASE_NAME ='DoubleT-TniCiu'
