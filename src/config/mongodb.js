5bc350b090342231f5e08853c1b2841a8c8ceadd
TniCiu <tindoris29@gmail.com>
2024-04-18 20:55:06 +0700

    Properly Configure MongoDB+ Node in ENV

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,15 +1,12 @@
-
-const MONGODB_URI = 'mongodb+srv://tincui012:Tin2003@cluster0-tniciu.zokbtjy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0-TniCiu'
-
-const DATABASE_NAME ='DoubleT-TniCiu'
-
 import { MongoClient,ServerApiVersion } from 'mongodb'
+import {env} from '~/config/environment'
+
 
 // Khởi tạo một đối tượng DoubleTDatabaseInstance ban đầu là null (vì tôi chưa connect)
 let DoubleTDatabaseInstance = null
 
 // Khởi tạo một đối tượng mongoClientInstance để connect tới MongoDB
-const mongoClientInstance = new MongoClient(MONGODB_URI,{
+const mongoClientInstance = new MongoClient(env.MONGODB_URI,{
     // ServerApi: {
     //     version: ServerApiVersion.v1,
     //     strict: true,
@@ -22,7 +19,7 @@ export const CONNECT_DB = async () =>{
     await mongoClientInstance.connect()
 
     // kết nối thành công thì lấy ra database theo tên và gán ngược nó lại biến DoubleTDatabaseInstance ở trên 
-    DoubleTDatabaseInstance = mongoClientInstance.db(DATABASE_NAME)
+    DoubleTDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME)
 
 }
 
--- src/server.js
+++ src/server.js
@@ -1,22 +1,20 @@
 import express from 'express'
 import exitHook from 'async-exit-hook'
-import { CONNECT_DB, GET_DB,CLOSE_DB } from '~/config/mongodb'
+import { CONNECT_DB,CLOSE_DB } from '~/config/mongodb'
+import {env} from '~/config/environment'
+
 
 const START_SERVER = () => {
   const app = express()
 
-  const hostname = 'localhost'
-  const port = 2903
-
   app.get('/', async (req, res) => {
-    console.log(await GET_DB().listCollections().toArray() )
 
     res.end('<h1>Hello World!</h1><hr>');
     
     })
   
-  app.listen(port, hostname, () => {
-    console.log(`3. Hello Huynh Trong Tin, Back-end Server is running successfully at http://${hostname}:${port}/
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    console.log(`3. Hello ${env.AUTHOR}, Back-end Server is running successfully at http://${env.APP_HOST}:${env.APP_PORT}/
     `)
   })
 
