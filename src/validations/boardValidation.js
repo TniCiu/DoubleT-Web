a15bf9525672fb4f3a9c468f0d978fffb6d9eb64
TniCiu <tindoris29@gmail.com>
2024-04-22 11:08:26 +0700

    Define the Collection Schema in the Model tier

--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -13,7 +13,7 @@ const createNew = async (req, res, next) => {
      */
     const correctCondition = Joi.object({
         title: Joi.string().required().min(3).max(50).trim().strict(),
-        description: Joi.string().required().min(3).max(256).trim().strict(),
+        description: Joi.string().required().min(3).max(256).trim().strict()
     })
     try {
         
